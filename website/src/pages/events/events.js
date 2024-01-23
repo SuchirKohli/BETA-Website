e9415a419fe50100082ad4458ff78ad10a5db1c0
Suchir <suchirkohli04@gmail.com>
2024-01-23 23:13:51 +0530

    Events1.0

--- website/src/pages/events/events.js
+++ website/src/pages/events/events.js
@@ -1,8 +1,15 @@
 import React from "react";
+// import AltTimeline from "./timeline";
+import HiFiTimeline from "./hifitimeline";
 
 function Events() {
-  return (
-    <h1>Events</h1>
-  );
+  return(
+  <div>
+    <div class="d-flex justify-content-center p-5 bg-warning"><h1>Upcoming Events</h1></div>
+    {/* <AltTimeline /> */}
+    <HiFiTimeline />
+    <div class="d-flex justify-content-center p-5 bg-warning"><h1>Events History</h1></div>
+  </div>
+    )
 }
 export default Events;
